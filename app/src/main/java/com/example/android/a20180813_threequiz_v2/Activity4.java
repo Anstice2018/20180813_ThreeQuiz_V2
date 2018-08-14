f94fa8243585eac242e9823b15983a498fee9276
Anstice <co674338no@gmail.com>
2018-08-14 14:30:02 +0800

    finish

--- app/src/main/java/com/example/android/a20180813_threequiz_v2/Activity4.java
+++ app/src/main/java/com/example/android/a20180813_threequiz_v2/Activity4.java
@@ -3,24 +3,42 @@ package com.example.android.a20180813_threequiz_v2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class Activity4 extends AppCompatActivity {
 
+    private TextView ans;
+    private CharSequence q1Answer;
+    private CharSequence q2Answer;
+    private CharSequence q3Answer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_4);
+        setContentView(R.layout.activity_result);
+
+        ans = findViewById(R.id.textView2);
 
         Intent intent = getIntent();
-        TextView ans = findViewById(R.id.textView2);
+        q1Answer = intent.getCharSequenceExtra(Activity1.Q1_KEY);
+        q2Answer = intent.getCharSequenceExtra(Activity2.Q2_KEY);
+        q3Answer = intent.getCharSequenceExtra(Activity3.Q3_KEY);
 
-        CharSequence q1Answer = intent.getCharSequenceExtra(Activity1.Q1_KEY);
-        CharSequence q2Answer = intent.getCharSequenceExtra(Activity2.Q2_KEY);
-        CharSequence q3Answer = intent.getCharSequenceExtra(Activity3.Q3_KEY);
+        StringBuilder result = new StringBuilder();
+        result.append("1. ").append(q1Answer).append("\n")
+                .append("2. ").append(q2Answer).append("\n")
+                .append("3. ").append(q3Answer);
+        ans = findViewById(R.id.textView2);
+        ans.setText(result);
 
-        ans.setText(q1Answer + "\n" + q2Answer + "\n" + q3Answer);
+    }
 
 
+
+    public void clickBack (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.push_in, R.anim.push_out);
     }
 }
